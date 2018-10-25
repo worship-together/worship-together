602bb2f2245b303cbe3f23c99e3a2f2ed2aa4fa3
Michael Hewitt <michael@hewitts.us>
2018-10-24 21:49:59 -0700

    fix song tempos

--- events.py
+++ events.py
@@ -12,101 +12,100 @@ default_velocity = 60
 
 
 class Event():
-    def __init__(self, tick=0):
-        self.tick = tick
-
-    def to_bytes(self):
-        tick = _get_var_len(int(self.tick))
-        return tick
-
-
+	def __init__(self, tick=0):
+		self.tick = tick
+		
+	def to_bytes(self):
+		tick = _get_var_len(int(self.tick))
+		return tick
+		
+		
 class NoteEvent(Event):
-    def __init__(self, voice, tick, pitch):
-        super().__init__(tick)
-        self.voice = voice
-        self.pitch = pitch
-        self.velocity = default_velocity
-
-    def __repr__(self):
-        return f'({self.voice.name:7} {self.tick:4}, {self.pitch:2}, ' \
-               f'{"On" if type(self) == NoteOnEvent else "Off"})'
-
-    def code(self):
-        raise NotImplementedError('must override code method')
-
-    def to_bytes(self):
-        code = self.code()
-        channel = self.voice.value
-        sts_msg = struct.pack('B', (code & 0xf0) + (channel & 0x0f))
-        pitch = struct.pack('B', self.pitch)
-        velocity = struct.pack('B', self.velocity)
-        return super().to_bytes() + sts_msg + pitch + velocity
-
-
+	def __init__(self, voice, tick, pitch):
+		super().__init__(tick)
+		self.voice = voice
+		self.pitch = pitch
+		self.velocity = default_velocity
+		
+	def __repr__(self):
+		return f'({self.voice.name:7} {self.tick:4}, {self.pitch:2}, ' \
+		f'{"On" if type(self) == NoteOnEvent else "Off"})'
+		
+	def code(self):
+		raise NotImplementedError('must override code method')
+		
+	def to_bytes(self):
+		code = self.code()
+		channel = self.voice.value
+		sts_msg = struct.pack('B', (code & 0xf0) + (channel & 0x0f))
+		pitch = struct.pack('B', self.pitch)
+		velocity = struct.pack('B', self.velocity)
+		return super().to_bytes() + sts_msg + pitch + velocity
+		
+		
 class NoteOnEvent(NoteEvent):
-    def code(self):
-        return 0x90
-
-
+	def code(self):
+		return 0x90
+		
+		
 class NoteOffEvent(NoteEvent):
-    def code(self):
-        return 0x80
-
-
+	def code(self):
+		return 0x80
+		
+		
 class RestEvent(Event):
-    pass
-
-
+	pass
+	
+	
 class MetaEvent(Event):
-    def __init__(self, code, data):
-        super().__init__()
-        self.code = code
-        self.data = data
-
-    def to_bytes(self):
-        code_bytes = struct.pack('BB', 0xff, self.code)
-        data_len = struct.pack('B', len(self.data))
-        data_bytes = array.array('B', self.data).tobytes()
-        return super().to_bytes() + code_bytes + data_len + data_bytes
-
-
+	def __init__(self, code, data):
+		super().__init__()
+		self.code = code
+		self.data = data
+		
+	def to_bytes(self):
+		code_bytes = struct.pack('BB', 0xff, self.code)
+		data_len = struct.pack('B', len(self.data))
+		data_bytes = array.array('B', self.data).tobytes()
+		return super().to_bytes() + code_bytes + data_len + data_bytes
+		
+		
 class SmpteOffsetEvent(MetaEvent):
-    def __init__(self):
-        super().__init__(code=0x54, data=[0, 0, 0, 0, 0])
-
-
+	def __init__(self):
+		super().__init__(code=0x54, data=[0, 0, 0, 0, 0])
+		
+		
 class TimeSignatureEvent(MetaEvent):
-    def __init__(self):
-        super().__init__(code=0x58, data=[4, 2, 24, 8])
-
-
+	def __init__(self):
+		super().__init__(code=0x58, data=[4, 2, 24, 8])
+		
+		
 class KeySignatureEvent(MetaEvent):
-    def __init__(self):
-        super().__init__(code=0x59, data=[0, 0])
-
-
+	def __init__(self):
+		super().__init__(code=0x59, data=[0, 0])
+		
+		
 class SetTempoEvent(MetaEvent):
-    def __init__(self, beats_per_minute=180):
-        seconds_per_beat = 60 / beats_per_minute
-        microseconds_per_beat = int(seconds_per_beat * 1000000)
-        data_0 = microseconds_per_beat & 0xFF
-        data_1 = (microseconds_per_beat & 0xFF00) >> 8
-        data_2 = (microseconds_per_beat & 0xFF0000) >> 16
-        super().__init__(code=0x51, data=[data_2, data_1, data_0])
-
-
+	def __init__(self, beats_per_minute):
+		seconds_per_beat = 60 / beats_per_minute
+		microseconds_per_beat = int(seconds_per_beat * 1000000)
+		data_0 = microseconds_per_beat & 0xFF
+		data_1 = (microseconds_per_beat & 0xFF00) >> 8
+		data_2 = (microseconds_per_beat & 0xFF0000) >> 16
+		super().__init__(code=0x51, data=[data_2, data_1, data_0])
+		
+		
 class EndOfTrackEvent(MetaEvent):
-    def __init__(self):
-        super().__init__(code=0x2F, data=[])
-        self.tick = 34
-
+	def __init__(self):
+		super().__init__(code=0x2F, data=[])
+		self.tick = 34
+		
 def _get_var_len(value, low_order_byte=True):
-    prefix_bit = (0 if low_order_byte else 0x80)
-    byte = struct.pack('B', prefix_bit | (value & 0x7f))
-    shifted = value >> 7
-    if shifted > 0:
-        return _get_var_len(shifted, low_order_byte=False) + byte
-    else:
-        return byte
-
+	prefix_bit = (0 if low_order_byte else 0x80)
+	byte = struct.pack('B', prefix_bit | (value & 0x7f))
+	shifted = value >> 7
+	if shifted > 0:
+		return _get_var_len(shifted, low_order_byte=False) + byte
+	else:
+		return byte
 
