d8563999b6975c7e9effa0ab0a16c94f54b3ab5e
Michael Hewitt <michael@hewitts.us>
2018-10-28 21:08:17 -0700

    CC songs 100, 368, 280, additional keys, accidentals

--- keys.py
+++ keys.py
@@ -1,9 +1,5 @@
 from notes import *
 
-Fs = {        F1:F1s, F2:F2s, F3:F3s, F4:F4s, F5:F5s, F6:F6s, F7:F7s}
-
-Bb = {B0:B0b, B1:B1b, B2:B2b, B3:B3b, B4:B4b, B5:B5b, B6:B6b, B7:B7b}
-Eb = {        E1:E1b, E2:E2b, E3:E3b, E4:E4b, E5:E5b, E6:E6b, E7:E7b}
 
 class Key:
 	def __init__(self):
@@ -29,10 +25,16 @@ class D_Flat(Key):
 		raise NotImplemented()
 		
 		
-class D(Key):
+class G(Key):
 	def __init__(self):
 		super().__init__()
-		raise NotImplemented()
+		self.notes.update({F1:F1s, F2:F2s, F3:F3s, F4:F4s, F5:F5s, F6:F6s, F7:F7s})
+		
+		
+class D(G):
+	def __init__(self):
+		super().__init__()
+		self.notes.update({C1:C1s, C2:C2s, C3:C3s, C4:C4s, C5:C5s, C6:C6s, C7:C7s})
 		
 		
 class D_Sharp(Key):
@@ -68,7 +70,7 @@ class F_Flat(Key):
 class F(Key):
 	def __init__(self):
 		super().__init__()
-		self.notes.update(Bb)
+		self.notes.update({B0:B0b, B1:B1b, B2:B2b, B3:B3b, B4:B4b, B5:B5b, B6:B6b, B7:B7b})
 		
 		
 class F_Sharp(Key):
@@ -81,12 +83,6 @@ class G_Flat(Key):
 	def __init__(self):
 		super().__init__()
 		raise NotImplemented()
-		
-		
-class G(Key):
-	def __init__(self):
-		super().__init__()
-		self.notes.update(Fs)
 		
 		
 class G_Sharp(Key):
@@ -113,11 +109,10 @@ class A_Sharp(Key):
 		raise NotImplemented()
 		
 		
-class B_Flat(Key):
+class B_Flat(F):
 	def __init__(self):
 		super().__init__()
-		self.notes.update(Bb)
-		self.notes.update(Eb)
+		self.notes.update({E1:E1b, E2:E2b, E3:E3b, E4:E4b, E5:E5b, E6:E6b, E7:E7b})
 		
 		
 class B(Key):
---
+++ songs/cc_100.py
@@ -0,0 +1,87 @@
+from notes import *
+import keys
+
+name = "God Shall Arise and by His Might"
+author = 'W. W. J. VanOene, 1972'
+tune = 'QUE DIEU SE MONSTRE SEULEMENT (8 8 7, 8 8 7, 8 8 7, 8 8 7)'
+composer = "Strasbourg, 1539"
+harmonies = "Claude Goudimel, 1564; alt."
+number = 'CC 100'
+beats_per_measure = 12
+tempo = 180
+key = keys.D
+
+measures = [
+    [
+        [D4(2), D4, E4, F4, D4, F4, G4, A4(3), R],
+        [A3(2), B3, C4, D4, A3, D4, E4, F4(3), R],
+        [D3(2), F3, A3, A3, F3, B3, B3, D4(3), R],
+        [D3(2), B2, A2, D3, D3, B2, E3, D3(3), R]
+    ],
+    [
+        [A4(2), G4, F4, E4, D4, F4, G4, A4(3), R],
+        [F4(2), D4, D4, C4, B3, D4, E4, F4(3), R],
+        [D4(2), B3, A3, A3, F3, B3, B3, D4(3), R],
+        [D3(2), G2, D3, A2, B2, B2, E3, D3(3), R]
+    ],
+    [
+        [B4(2), A4, G4, F4, D4, E4(2), D4(3), R],
+        [G4(2), F4, E4, D4, D4(2), C4, D4(3), R],
+        [D4(2), D4, B3, B3, F3, A3(2), F3(3), R],
+        [G2(2), D3, E3, B2, B2, A2(2), D3(3), R]
+    ],
+    [
+        [D4(2), D4, E4, F4, D4, F4, G4, A4(3), R],
+        [A3(2), B3, C4, D4, A3, D4, E4, F4(3), R],
+        [D3(2), F3, A3, A3, F3, B3, B3, D4(3), R],
+        [D3(2), B2, A2, D3, D3, B2, E3, D3(3), R]
+    ],
+    [
+        [A4(2), G4, F4, E4, D4, F4, G4, A4(3), R],
+        [F4(2), D4, D4, C4, B3, D4, E4, F4(3), R],
+        [D4(2), B3, A3, A3, F3, B3, B3, D4(3), R],
+        [D3(2), G2, D3, A2, B2, B2, E3, D3(3), R]
+    ],
+    [
+        [B4(2), A4, G4, F4, D4, E4(2), D4(3), R],
+        [G4(2), F4, E4, D4, D4(2), C4, D4(3), R],
+        [D4(2), D4, B3, B3, F3, A3(2), F3(3), R],
+        [G2(2), D3, E3, B2, B2, A2(2), D3(3), R]
+    ],
+    [
+        [D5(2), D5, D5, C5, A4, B4, B4, A4(3), R],
+        [F4(2), F4, F4, E4, C4, E4, E4, C4(3), R],
+        [A3(2), B3, A3, A3, A3, A3, G3s, A3(3), R],
+        [D3(2), B2, D3, A2, A2, E3, E3, A2(3), R]
+    ],
+    [
+        [D5(2), D5, D5, C5, A4, B4, B4, A4(3), R],
+        [F4(2), F4, F4, E4, C4, E4, E4, C4(3), R],
+        [A3(2), B3, A3, A3, A3, A3, G3s, A3(3), R],
+        [D3(2), B2, D3, A2, A2, E3, E3, A2(3), R]
+    ],
+    [
+        [F4(2), G4, B4, A4, F4, G4(2), F4(3), R],
+        [D4(2), D4, G4, F4, F4, E4(2), D4s(3), R],
+        [A3(2), B3, D4, D4, D4, B3(2), B3(3), R],
+        [D3(2), G2, G2, D3, D3, E3(2), B2(3), R]
+    ],
+    [
+        [E4(2), F4, G4, A4, G4, F4, F4, E4(3), R],
+        [E4(2), D4, E4, F4, E4, E4, D4s, E4(3), R],
+        [B3(2), B3, B3, D4, B3, B3, B3, B3(3), R],
+        [E3(2), B2, E3, D3, E3, B2, B2, E3(3), R]
+    ],
+    [
+        [A4(2), B4, C5, D5, C5, B4, B4, A4(3), R],
+        [F4(2), F4, A4, A4, A4, A4, G4s, A4(3), R],
+        [D4(2), D4, E4, F4, E4, E4, E4, C4(3), R],
+        [D3(2), B2, A2, D3, A2, E3, E3, A2(3), R]
+    ],
+    [
+        [D4(2), E4, F4, G4, F4, E4(2), D4(4)],
+        [C4(2), E4, D4, D4, D4(2), C4, D4(4)],
+        [F3(2), A3, A3, B3, A3, A3(2), A3(4)],
+        [D3(2), C3, D3, G2, D3, A2(2), D3(4)]
+    ]
+]
